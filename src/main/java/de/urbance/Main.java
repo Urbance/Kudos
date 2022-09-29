2d15f3031a46dc466ad3acb300fca59031a6e0c7
Martin <schmidtmartin697@gmail.com>
2022-09-29 20:02:27 +0200

    feat: Build config file

--- src/main/java/de/urbance/Main.java
+++ src/main/java/de/urbance/Main.java
@@ -1,17 +1,25 @@
 package de.urbance;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    FileConfiguration config = getConfig();
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        config.options().copyDefaults(true);
+        saveConfig();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public FileConfiguration getConfigFile() {
+        return getConfig();
     }
 }
