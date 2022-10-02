db785268f9e1e1d4cbc8e006a0a1f5491337711b
Martin <schmidtmartin697@gmail.com>
2022-10-02 21:10:33 +0200

    feat: Build up dummy SQL connection

--- src/main/java/de/urbance/Main.java
+++ src/main/java/de/urbance/Main.java
@@ -2,12 +2,17 @@ package de.urbance;
 
 import Commands.Kudos;
 import Utils.GUI;
+import Utils.SQL;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.SQLException;
+
 public final class Main extends JavaPlugin {
+
+    public Utils.SQL SQL;
 
     FileConfiguration config = getConfig();
 
@@ -21,11 +26,24 @@ public final class Main extends JavaPlugin {
         PluginManager pluginManager = Bukkit.getPluginManager();
         pluginManager.registerEvents(new GUI(), this);
         getCommand("kudos").setExecutor(new Kudos());
+
+        // SQL
+        this.SQL = new SQL();
+        try {
+            SQL.connect();
+        } catch (ClassNotFoundException | SQLException e) {
+            Bukkit.getLogger().info("Database not connected");
+        }
+
+        if (SQL.isConnected()) {
+            Bukkit.getLogger().info("Database is connected");
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        SQL.disconnect();
     }
 
     public FileConfiguration getConfigFile() {
