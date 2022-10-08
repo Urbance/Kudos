cc5df1c2665f7639df16a5e3e3a766473ab3de68
Martin <schmidtmartin697@gmail.com>
2022-10-08 17:56:30 +0200

    fix: Improved Kudos error handling, fix "wrong_usage" message

--- src/main/java/Commands/Kudos.java
+++ src/main/java/Commands/Kudos.java
@@ -13,44 +13,61 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
+import static de.urbance.Main.prefix;
+
 public class Kudos implements CommandExecutor {
     public static Inventory inventory;
-
+    public FileConfiguration locale;
     public SQLGetter data;
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player player = ((Player) sender).getPlayer();
-            String prefix = Main.prefix;
-            FileConfiguration locale = new LocaleManager(Main.getPlugin(Main.class)).getConfig();
-
-            if (args.length == 0) {
-                if (player.hasPermission("kudos.gui") || player.hasPermission("kudos.*")) {
-                    inventory = new GUI().getInventory();
-                    player.openInventory(inventory);
-                } else {
-                    Bukkit.getPlayer(player.getName()).sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + locale.getString("error.no_permission")));
-                }
-            }
-
-            if (args.length == 1) {
-                data = new SQLGetter(Main.getPlugin(Main.class));
-
-                if (player.hasPermission("kudos.show") || player.hasPermission("kudos.*")) {
-                    Player targetPlayer = Bukkit.getPlayer(args[0]);
-
-                    String showKudosMessage = locale.getString("kudos.show_player_kudos");
-                    showKudosMessage = showKudosMessage.replaceAll("%targetplayer%", targetPlayer.getName());
-                    showKudosMessage = showKudosMessage.replaceAll("%targetplayer_kudos%", String.valueOf(data.getKudos(targetPlayer.getUniqueId())));
-                    Bukkit.getPlayer(player.getName()).sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + showKudosMessage));
-                } else {
-                    Bukkit.getPlayer(player.getName()).sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + locale.getString("error.no_permission")));
-                }
-            }
-        } else {
+        if (!(sender instanceof Player)) {
             Bukkit.getServer().getLogger().info("You can't execute this command as console!");
+            return false;
         }
+
+        locale = new LocaleManager(Main.getPlugin(Main.class)).getConfig();
+
+        if (args.length > 1) {
+            Bukkit.getPlayer(sender.getName()).sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + locale.getString("error.wrong_usage")));
+            return false;
+        }
+
+        if (args.length == 0) {
+            openGUI(sender);
+        }
+
+        if (args.length == 1) {
+            showKudos(args, sender);
+        }
+
         return false;
+    }
+
+    public void openGUI(CommandSender sender) {
+        if (!(sender.hasPermission("kudos.gui") || sender.hasPermission("kudos.*"))) {
+            Bukkit.getPlayer(sender.getName()).sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + locale.getString("error.no_permission")));
+            return;
+        }
+
+        Player player = Bukkit.getPlayer(sender.getName());
+        inventory = new GUI().getInventory();
+        player.openInventory(inventory);
+    }
+
+    public void showKudos(String[] args, CommandSender sender) {
+        data = new SQLGetter(Main.getPlugin(Main.class));
+
+        if (!(sender.hasPermission("kudos.show") || sender.hasPermission("kudos.*"))) {
+            Bukkit.getPlayer(sender.getName()).sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + locale.getString("error.no_permission")));
+            return;
+        }
+
+        Player targetPlayer = Bukkit.getPlayer(args[0]);
+        String showKudosMessage = locale.getString("kudos.show_player_kudos");
+        showKudosMessage = showKudosMessage.replaceAll("%targetplayer%", targetPlayer.getName());
+        showKudosMessage = showKudosMessage.replaceAll("%targetplayer_kudos%", String.valueOf(data.getKudos(targetPlayer.getUniqueId())));
+        Bukkit.getPlayer(sender.getName()).sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + showKudosMessage));
     }
 }
