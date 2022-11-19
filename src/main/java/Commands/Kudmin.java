8ba7a588e29d66afa394d513a7495a1cc87910c7
Martin <schmidtmartin697@gmail.com>
2022-11-19 01:14:51 +0100

    fix(Kudmin): Improved feedback in case of incorrect command input
    -> in case of wrong input you get now feedback, what has been entered incorrectly
    -> values can now be 0
    -> clean code and refactoring

--- src/main/java/Commands/Kudmin.java
+++ src/main/java/Commands/Kudmin.java
@@ -15,7 +15,6 @@ import org.bukkit.plugin.PluginDescriptionFile;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class Kudmin implements CommandExecutor, TabCompleter {
     public String prefix = "&7[&cKudmin&7] ";
@@ -102,37 +101,55 @@ public class Kudmin implements CommandExecutor, TabCompleter {
                data.clearKudos(Bukkit.getOfflinePlayer(args[1]).getUniqueId());
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "Cleared Kudos from &e" + args[1]));
                break;
-       }
+
+            default:
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "Unknown argument &e" + args[0] + "&7. Type &e/kudmin help &7to get more informations!"));
+        }
     return false;
     }
 
     private boolean validateInput(String[] args, CommandSender sender) {
-        SQLGetter data = new SQLGetter(plugin);
-
-        if (args.length < 3) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "Wrong usage! Please try &e/kudmin help"));
+        if (!ifTargetPlayerExists(sender, args)) {
             return false;
         }
-
-        try {
-            Integer.parseInt(args[2]);
-        } catch (NumberFormatException e) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "Please enter a positive integer number"));
-            return false;
-        }
-
-        if (Integer.parseInt(args[2]) < 0) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "Please enter a number that is greater than 0"));
-            return false;
-        }
-
-        OfflinePlayer player = data.getPlayer(Bukkit.getOfflinePlayer(args[1]).getUniqueId());
-        if (player == null) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "Wrong usage! Player &e" + args[1] + " &7not found"));
+        if (!isValueAnInteger(sender, args)) {
             return false;
         }
         return true;
     }
+
+    private boolean isValueAnInteger(CommandSender sender, String[] args) {
+        if (args.length < 3 || !isValueAnInteger(args[2]) || !(Integer.parseInt(args[2]) >= 0)) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "Please enter a positive integer number!"));
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValueAnInteger(String rawValue) {
+        try {
+            Integer.parseInt(rawValue);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean ifTargetPlayerExists(CommandSender sender, String[] args) {
+        SQLGetter data = new SQLGetter(plugin);
+        if (args.length < 2) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "Please enter a target player!"));
+            return false;
+        }
+        String targetPlayerName = args[1];
+        OfflinePlayer targetPlayer = data.getPlayer(Bukkit.getOfflinePlayer(targetPlayerName).getUniqueId());
+        if (targetPlayer == null) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + "Player &e" + targetPlayerName + " &7not found!"));
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
         ArrayList<String> list = new ArrayList<>();
