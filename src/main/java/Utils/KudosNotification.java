9fe8bef80c004a2c640cd01c69018a8d36e86583
Martin <schmidtmartin697@gmail.com>
2023-01-31 22:09:52 +0100

    chore: replaced old placeholder names with new placeholder names

--- src/main/java/Utils/KudosNotification.java
+++ src/main/java/Utils/KudosNotification.java
@@ -28,17 +28,17 @@ public class KudosNotification {
     public void fromConsole(Player targetPlayer) {
         UUID targetPlayerUUID = targetPlayer.getUniqueId();
         Map<String, String> values = new HashMap<>();
-        values.put("targetplayer", targetPlayer.getName());
-        values.put("player_kudos", String.valueOf(data.getKudos(targetPlayerUUID)));
+        values.put("kudos_targetplayer_name", targetPlayer.getName());
+        values.put("kudos_targetplayer_kudos", String.valueOf(data.getKudos(targetPlayerUUID)));
         kudosMessage.broadcast(kudosMessage.setPlaceholders(locale.getString("kudo.player-award-kudo-from-console"), values));
     }
 
     public void sendBroadcast(CommandSender sender, Player targetPlayer) {
         UUID targetPlayerUUID = targetPlayer.getUniqueId();
         Map<String, String> values = new HashMap<>();
-        values.put("player", sender.getName());
-        values.put("targetplayer", targetPlayer.getName());
-        values.put("player_kudos", String.valueOf(data.getKudos(targetPlayerUUID)));
+        values.put("kudos_player_name", sender.getName());
+        values.put("kudos_targetplayer_name", targetPlayer.getName());
+        values.put("kudos_targetplayer_kudos", String.valueOf(data.getKudos(targetPlayerUUID)));
         kudosMessage.broadcast(kudosMessage.setPlaceholders(locale.getString("kudo.player-award-kudo-broadcast"), values));
     }
 
@@ -47,13 +47,13 @@ public class KudosNotification {
 
         // send message to sender
         Map<String, String> valuesSender = new HashMap<>();
-        valuesSender.put("targetplayer", targetPlayer.getName());
+        valuesSender.put("kudos_targetplayer_name", targetPlayer.getName());
         kudosMessage.sendSender(sender, kudosMessage.setPlaceholders(locale.getString("kudo.player-assigned-kudo"), valuesSender));
 
         // send message to target player
         Map<String, String> valuesTargetPlayer = new HashMap<>();
-        valuesTargetPlayer.put("player", sender.getName());
-        valuesTargetPlayer.put("player_kudos", String.valueOf(data.getKudos(targetPlayerUUID)));
+        valuesTargetPlayer.put("kudos_player_name", sender.getName());
+        valuesTargetPlayer.put("kudos_targetplayer_kudos", String.valueOf(data.getKudos(targetPlayerUUID)));
         kudosMessage.send(targetPlayer, kudosMessage.setPlaceholders(locale.getString("kudo.player-award-kudo-from-player"), valuesTargetPlayer));
     }
 
