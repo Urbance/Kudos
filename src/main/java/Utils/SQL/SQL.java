1fc240604ec71f98612b04cf7680074f6af48af3
Martin <schmidtmartin697@gmail.com>
2023-06-02 22:53:48 +0200

    style(SQL): clean code

--- src/main/java/Utils/SQL/SQL.java
+++ src/main/java/Utils/SQL/SQL.java
@@ -39,17 +39,17 @@ public class SQL {
     public void connect() throws  ClassNotFoundException, SQLException {
         if (!isConnected()) {
             HikariDataSource dataSource = new HikariDataSource();
+            dataSource.addDataSourceProperty("prepStmtCacheSize", "250");
+            dataSource.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+            dataSource.addDataSourceProperty("cachePrepStmts", "true");
+            dataSource.addDataSourceProperty("useServerPrepStmts", "true");
+            dataSource.addDataSourceProperty("keepaliveTime", "30000");
+            dataSource.addDataSourceProperty("maxLifetime", "1800000");
             if (config.getBoolean("general.use-SQL")) {
                 dataSource.setJdbcUrl(String.format("jdbc:mysql://%s:%s/%s?&useSSL=%s", host, port, database, useSSL));
                 dataSource.setUsername(username);
                 dataSource.setPassword(password);
                 dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-                dataSource.addDataSourceProperty("prepStmtCacheSize", "250");
-                dataSource.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-                dataSource.addDataSourceProperty("cachePrepStmts", "true");
-                dataSource.addDataSourceProperty("useServerPrepStmts", "true");
-                dataSource.addDataSourceProperty("keepaliveTime", "30000");
-                dataSource.addDataSourceProperty("maxLifetime", "1800000");
             } else {
                 String dataFolderPath = (String.format("%s/data", plugin.getDataFolder()));
                 createDataFolder(dataFolderPath);
