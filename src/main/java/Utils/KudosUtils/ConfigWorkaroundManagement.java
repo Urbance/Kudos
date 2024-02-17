06a4459f68e886810dba83700c0542429f1b791e
Martin <schmidtmartin697@gmail.com>
2024-02-17 22:14:42 +0100

    style: move gui config into gui folder

--- src/main/java/Utils/KudosUtils/ConfigWorkaroundManagement.java
+++ src/main/java/Utils/KudosUtils/ConfigWorkaroundManagement.java
@@ -11,58 +11,59 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 public class ConfigWorkaroundManagement {
     private Main plugin;
-    private FileManager guiConfigManager;
-    private FileConfiguration guiConfig;
 
     public ConfigWorkaroundManagement() {
         this.plugin = Main.getPlugin(Main.class);
-        this.guiConfigManager = new FileManager("gui.yml", plugin);
-        this.guiConfig = guiConfigManager.getConfig();
     }
 
     public void performWorkarounds() {
         // 4.3.0 Minor Update
         // move received kudos page switcher to general key
+
+        FileManager guiConfigManagaerDeprecated = new FileManager("gui.yml", plugin);
+        FileConfiguration guiConfigDeprecated = guiConfigManagaerDeprecated.getConfig();
+
         String receivedKudosBackwardsItemNameKeyPath = "received-kudos.backwards-item.item-name";
         String receivedKudosForwardsItemNameKeyPath = "received-kudos.forwards-item.item-name";
 
         String generalPageSwitcherBackwardsItemNameKeyPath = "general.page-switcher.backwards.item-name";
         String generalPageSwitcherForwardsItemNameKeyPath = "general.page-switcher.forwards.item-name";
 
-        if (guiConfig.getString(receivedKudosBackwardsItemNameKeyPath) != null) {
-            guiConfig.set(generalPageSwitcherBackwardsItemNameKeyPath, guiConfig.getString(receivedKudosBackwardsItemNameKeyPath));
-            guiConfig.set("received-kudos.backwards-item", null);
+        if (guiConfigDeprecated.getString(receivedKudosBackwardsItemNameKeyPath) != null) {
+            guiConfigDeprecated.set(generalPageSwitcherBackwardsItemNameKeyPath, guiConfigDeprecated.getString(receivedKudosBackwardsItemNameKeyPath));
+            guiConfigDeprecated.set("received-kudos.backwards-item", null);
         }
 
-        if (guiConfig.getString(receivedKudosForwardsItemNameKeyPath) != null) {
-            guiConfig.set(generalPageSwitcherForwardsItemNameKeyPath, guiConfig.getString(receivedKudosForwardsItemNameKeyPath));
-            guiConfig.set("received-kudos.forwards-item", null);
+        if (guiConfigDeprecated.getString(receivedKudosForwardsItemNameKeyPath) != null) {
+            guiConfigDeprecated.set(generalPageSwitcherForwardsItemNameKeyPath, guiConfigDeprecated.getString(receivedKudosForwardsItemNameKeyPath));
+            guiConfigDeprecated.set("received-kudos.forwards-item", null);
         }
 
         // refactored papi leaderboard config key values into a separated key
         String oldNotAssignedKudosPapiKeyPath = "slot.kudos-leaderboard.not-assigned-kudos";
         String oldLoreFormatPapiKeyPath = "slot.kudos-leaderboard.lore-format";
 
-        if (guiConfig.getString(oldNotAssignedKudosPapiKeyPath) != null) {
-            String oldNotAssignedKudosPapiKeyValue = guiConfig.getString(oldNotAssignedKudosPapiKeyPath);
+        if (guiConfigDeprecated.getString(oldNotAssignedKudosPapiKeyPath) != null) {
+            String oldNotAssignedKudosPapiKeyValue = guiConfigDeprecated.getString(oldNotAssignedKudosPapiKeyPath);
             String newNotAssignedKudosPapiPath = "slot.kudos-leaderboard.papi.not-assigned-kudos";
 
             Bukkit.getLogger().info(oldNotAssignedKudosPapiKeyValue);
 
-            guiConfig.set(newNotAssignedKudosPapiPath, oldNotAssignedKudosPapiKeyValue);
-            guiConfig.set(oldNotAssignedKudosPapiKeyPath, null);
+            guiConfigDeprecated.set(newNotAssignedKudosPapiPath, oldNotAssignedKudosPapiKeyValue);
+            guiConfigDeprecated.set(oldNotAssignedKudosPapiKeyPath, null);
         }
 
-        if (guiConfig.getString(oldLoreFormatPapiKeyPath) != null) {
-            String oldLoreFormatPapiKeyValue = guiConfig.getString("slot.kudos-leaderboard.lore-format");
+        if (guiConfigDeprecated.getString(oldLoreFormatPapiKeyPath) != null) {
+            String oldLoreFormatPapiKeyValue = guiConfigDeprecated.getString("slot.kudos-leaderboard.lore-format");
             String newLoreFormatPapiKeyPath = "slot.kudos-leaderboard.papi.lore-format";
 
-            guiConfig.set(newLoreFormatPapiKeyPath, oldLoreFormatPapiKeyValue);
-            guiConfig.set(oldLoreFormatPapiKeyPath, null);
+            guiConfigDeprecated.set(newLoreFormatPapiKeyPath, oldLoreFormatPapiKeyValue);
+            guiConfigDeprecated.set(oldLoreFormatPapiKeyPath, null);
         }
 
         // TODO replace with FileManager
-        guiConfigManager.save();
-        plugin.guiConfig = guiConfigManager.getConfig();
+        guiConfigManagaerDeprecated.save();
+        plugin.guiConfig = guiConfigManagaerDeprecated.getConfig();
+
     }
 }
